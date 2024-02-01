dfae352ae5dd30db667349f04967334e622b8c13
Margarita Zhentsova <mzhentsova@gmail.com>
2024-02-01 19:27:06 +0400

    upd

--- next.config.mjs
+++ next.config.mjs
@@ -6,13 +6,13 @@ const nextConfig = process.env.NODE_ENV === 'production' ? {
   assetPrefix: isProd ? 'https:///nedostatoksna/next_training/' : undefined,
   basePath: '/next_training',
   images: {
-    remotePatterns: ['via.placeholder.com'],
+    domains: ['via.placeholder.com'],
     unoptimized: true,
   },
 } : {
   basePath: '/next_training',
   images: {
-    remotePatterns: ['via.placeholder.com'],
+    domains: ['via.placeholder.com'],
   },
 };
 
