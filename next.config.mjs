cb91c3176be54dbc09267cab222cf88fe63f20ba
Margarita Zhentsova <mzhentsova@gmail.com>
2024-02-01 18:42:44 +0400

    upd

--- next.config.mjs
+++ next.config.mjs
@@ -5,11 +5,6 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   output: 'export',
   basePath: '/next_training',
-  cssModules: true,
-  images: {
-    domains: ['via.placeholder.com'],
-    unoptimized: true,
-  },
   assetPrefix: isProd ? 'https:///nedostatoksna/next_training' : undefined,
   reactStrictMode: true,
 }
