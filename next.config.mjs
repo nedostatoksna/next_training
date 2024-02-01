5de4279121a04c61b7ba9f7a1446e1eeb32fb729
Margarita Zhentsova <mzhentsova@gmail.com>
2024-02-01 19:13:22 +0400

    upd

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const repo = 'next_training'
 const assetPrefix = `/${repo}/`
 const basePath = `/${repo}`
