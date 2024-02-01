ff24314b10f70377cd204de92a2027b318d40eb4
Margarita Zhentsova <mzhentsova@gmail.com>
2024-02-01 18:46:28 +0400

    image unoptimased

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,9 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   output: 'export',
   basePath: '/next_training',
+  images: {
+    unoptimized: true,
+  },
   assetPrefix: isProd ? 'https:///nedostatoksna/next_training' : undefined,
   reactStrictMode: true,
 }
